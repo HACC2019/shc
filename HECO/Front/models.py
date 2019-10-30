c70f873365d1a036f8e289bcb7cd7bc57049a8df
kobeuyeda <kobeuyeda@outlook.com>
2019-10-29 20:02:22 -0700

    view file has been update

--- HECO/Front/models.py
+++ HECO/Front/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Raw_Data(models.Model):
+    Charge_Station_Name = models.CharField(max_length=800)
+    Session_Initiated_By = models.CharField(max_length=800)
+    Start_Time = models.CharField(max_length=800)
+     = models.CharField(max_length=800)
