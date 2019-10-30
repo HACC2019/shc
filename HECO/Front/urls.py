622791107aab5c5fe8eb6e1b2d7a665383a4529f
kobeuyeda <kobeuyeda@outlook.com>
2019-10-29 19:46:07 -0700

    updated settings and urls

--- HECO/Front/urls.py
+++ HECO/Front/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', include('Front.urls'))
+    path('', views.Home, name = 'Home'),
     path('admin/', admin.site.urls),
 ]
