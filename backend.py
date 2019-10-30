9da354b71d198d187d062c5ddc502b8327511712
Shane Parslow <shaneparslow808@gmail.com>
2019-10-29 23:03:22 -1000

    sanity checks on add_column

---
+++ backend.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+def makeTimeStamps():
+    maxTime =
+    minTime =
+
+def add_column(db, table, column, data):
+    """Set a column equal to a list, creating the column if it doesn't exist"""
+
+    # Sanity Checks #
+    if type(db) is not sqlite3.Connection:
+        raise TypeError("Expected sqlite3.Connection, got {}".format(type(db)))
+    if type(table) is not str:
+        raise TypeError("Expected string, got {}".format(type(table)))
+    if type(column) is not str:
+        raise TypeError("Expected string, got {}".format(type(column)))
+    if type(data) is not list:
+        raise TypeError("Expected list, got {}".format(type(data)))
+
