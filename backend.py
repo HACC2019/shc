321abc98c5ba9821db87140a93ca4920b9535cd3
JadeEngstrom <jade_eng@yahoo.com>
2019-10-30 19:32:44 -0500

    Sort MIn and Max start and end times

--- backend.py
+++ backend.py
@@ -1,3 +1,14 @@
+from UnixConverter import unix_Converter
+unix_Converter()
+import sqlite3
+
+con = sqlite3.connect('shc.db')
+cursorObj = con.cursor()
 def makeTimeStamps():
-    maxTime =
-    minTime =
+    max_time = cursorObj.execute("SELECT MAX([End Time]) FROM raw")
+    min_time =  cursorObj.execute("SELECT MIN([Start Time]) FROM raw")
+   # for i in max_time
+
+
+    print(min_time)
+    print(max_time)
