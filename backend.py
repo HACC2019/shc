beb531a676874e97ff792798c376901fd5528447
Shane Parslow <shaneparslow808@gmail.com>
2019-10-30 23:40:46 -1000

    inserts, but at end of table. kinda buggy.

--- backend.py
+++ backend.py
@@ -1,15 +1,37 @@
-import sqlite3
+import MySQLdb
+
+con = MySQLdb.connect(db="hacc", host="pf.parsl.dev", user="hacc", passwd="hacc2019")
 
 
-def add_column(db, table, column, data):
+def add_column(db, table, column, data, dtype="TEXT"):
     """Set a column equal to a list, creating the column if it doesn't exist"""
 
-    # Sanity Checks #
-    if type(db) is not sqlite3.Connection:
-        raise TypeError("Expected sqlite3.Connection, got {}".format(type(db)))
+    # Sanity checks #
+    if type(db) is not MySQLdb.connections.Connection:
+        raise TypeError("Expected MySQLdb.connections.Connection, got {}".format(type(db)))
     if type(table) is not str:
         raise TypeError("Expected string, got {}".format(type(table)))
     if type(column) is not str:
         raise TypeError("Expected string, got {}".format(type(column)))
     if type(data) is not list:
         raise TypeError("Expected list, got {}".format(type(data)))
+
+    con.autocommit(on=True)  # Make sure to actually save changes
+
+    # Try to drop column if it exists #
+    try:
+        con.query("ALTER TABLE {} DROP COLUMN {}".format(table, column))
+    except MySQLdb._exceptions.OperationalError:
+        pass
+
+    # Create column and add in data #
+    con.query("ALTER TABLE {} ADD {} {}".format(table, column, dtype))
+    datastring = ""
+    for value in data:  # Reformat to be of format [(value), (value2)]
+        datastring += "('{}'),".format(value)
+    datastring = datastring[:-1]  # Remove trailing comma
+    print("INSERT INTO {} ({}) VALUES {}".format(table, column, datastring))
+    con.query("INSERT INTO {} ({}) VALUES {};".format(table, column, datastring))
+
+
+add_column(con, "proc", "dhjskadhsakj", ["hi", "hi2"])
