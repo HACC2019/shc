c3f083a26e8162db5afb0929aa1079d50a0e1895
Ethan <eibanez11173@gmail.com>
2019-10-30 18:54:09 -1000

    10/30 Ethan

--- UnixConverter.py
+++ UnixConverter.py
@@ -4,26 +4,45 @@ import sqlite3
 con = sqlite3.connect('shc.db')
 cursorObj = con.cursor()
 
+global StartList
+global EndList
+
 def sql_fetch_StartTimes(con):
+    global StartList
     cursorObj.execute('SELECT "Start Time" FROM raw')
     StartTime = cursorObj.fetchall()
-    StartList=[]
+    StartList = []
     for row in StartTime:
-        StartList+=row
+        StartList += row
+
+
+
 
 def sql_fetch_EndTimes(con):
+    global EndList
     cursorObj.execute('SELECT "End Time" FROM raw')
     EndTime = cursorObj.fetchall()
-    EndList=[]
+    EndList = []
     for row in EndTime:
-        EndList+=row
+        EndList += row
 
 
-print(sql_fetch_EndTimes(con))
+#sql_fetch_StartTimes(con)
+#sql_fetch_EndTimes(con)
+print(StartList)
+print(EndList)
+#print(sql_fetch_EndTimes(con))
+#print(sql_fetch_StartTimes(con))
 
-#StartTime_List = sql_fetch_StartTimes(con)
+def convertStartTime():
+    global StartList
+    for i in StartList:
+        ConvertedStart_List = []
+        UnixStartTime = datetime.strptime(i, '%m-%d-%Y" "%I:%M:%S')
+        UnixStartTime += ConvertedStart_List
+        # return int(UnixStartTime.timestamp())
+        print(ConvertedStart_List)
 
-def convertUNIX():
-    now = datetime.now()
-    timestamp = datetime.timestamp(now)
-    print(timestamp)
+
+
+
