dbabd525f18bf2f2196e7089ed29f0952e2acbb3
kobeuyeda <kobeuyeda@outlook.com>
2019-10-30 14:44:29 -1000

    save database change

--- HECO/HECO/settings.py
+++ HECO/HECO/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'HECO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'HACC_HECO_mysql_Database.cnf'),
     }
 }
 
