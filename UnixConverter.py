e04f466b6a04add90dbe3d6c74f2a4b7ed696a44
JadeEngstrom <jade_eng@yahoo.com>
2019-10-30 19:11:00 -0500

    UnixConverter  10/30

--- UnixConverter.py
+++ UnixConverter.py
@@ -1,24 +1,48 @@
 from datetime import datetime
 import sqlite3
+global StartList
+global EndList
 
 con = sqlite3.connect('shc.db')
 cursorObj = con.cursor()
 
 def sql_fetch_StartTimes(con):
+    global StartList
     cursorObj.execute('SELECT "Start Time" FROM raw')
     StartTime = cursorObj.fetchall()
+    StartList=[]
     for row in StartTime:
-        print(row)
+        StartList+=row
 
 def sql_fetch_EndTimes(con):
+    global EndList
     cursorObj.execute('SELECT "End Time" FROM raw')
     EndTime = cursorObj.fetchall()
+    EndList=[]
     for row in EndTime:
-        print(row)
+        EndList+=row
 
-print(sql_fetch_StartTimes(con))
+sql_fetch_StartTimes(con)
+sql_fetch_EndTimes(con)
 
-def convertUNIX():
+'''def convertUNIX():
     now = datetime.now()
     timestamp = datetime.timestamp(now)
-    print(timestamp)
+    print(timestamp)'''
+for date in EndList:
+    try:
+        utc = datetime.strptime(date, '%m-%d-%y %H:%M:%S')
+        print(int(utc.timestamp()) - 36000)
+    except ValueError:
+        utc = datetime.strptime(date, '%m/%d/%Y %H:%M:%S')
+        print(int(utc.timestamp()) - 36000)
+
+for date in StartList:
+    try:
+        utc = datetime.strptime(date, '%m-%d-%y %H:%M:%S')
+        print(int(utc.timestamp()) - 36000)
+    except ValueError:
+        utc = datetime.strptime(date, '%m/%d/%Y %H:%M:%S')
+        print(int(utc.timestamp()) - 36000)
+
+
