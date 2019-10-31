cd2f92ba4f595f912a0b2eca0333b9da5af2b449
JadeEngstrom <jade_eng@yahoo.com>
2019-10-30 22:27:42 -0500

    `

--- UnixConverter.py
+++ UnixConverter.py
@@ -1,48 +1,84 @@
 from datetime import datetime
-import sqlite3
+import backend
+#import sqlite3
 global StartList
 global EndList
+global StartListInt
+global EndListInt
+import MySQLdb
 
-con = sqlite3.connect('shc.db')
+con = MySQLdb.connect(host="pf.parsl.dev", user="hacc", passwd="hacc2019", db='hacc')
+#con = sqlite3.connect('shc.db')
 cursorObj = con.cursor()
 
 def sql_fetch_StartTimes(con):
     global StartList
-    cursorObj.execute('SELECT "Start Time" FROM raw')
-    StartTime = cursorObj.fetchall()
+    con.query('SELECT Start_Time FROM raw')
+    StartTimeResult=con.store_result()
+    StartTime=StartTimeResult.fetch_row(maxrows=0)
     StartList=[]
     for row in StartTime:
-        StartList+=row
-
+        StartList.append(str(row))
 def sql_fetch_EndTimes(con):
     global EndList
-    cursorObj.execute('SELECT "End Time" FROM raw')
-    EndTime = cursorObj.fetchall()
+    #EndTime = cursorObj.execute('SELECT End_Time FROM raw')
+    con.query('SELECT End_Time FROM raw')
+    EndTimeResult = con.store_result()
+    EndTime=EndTimeResult.fetch_row(maxrows=0)
     EndList=[]
     for row in EndTime:
-        EndList+=row
+        EndList.append(str(row))
 
 sql_fetch_StartTimes(con)
 sql_fetch_EndTimes(con)
 
-'''def convertUNIX():
-    now = datetime.now()
-    timestamp = datetime.timestamp(now)
-    print(timestamp)'''
-for date in EndList:
-    try:
-        utc = datetime.strptime(date, '%m-%d-%y %H:%M:%S')
-        print(int(utc.timestamp()) - 36000)
-    except ValueError:
-        utc = datetime.strptime(date, '%m/%d/%Y %H:%M:%S')
-        print(int(utc.timestamp()) - 36000)
+def unix_Converter():
+    global StartListInt
+    global EndListInt
+    StartListInt=[]
+    EndListInt=[]
+    for date in StartList:
+        try:
+            utc = datetime.strptime(date, "('%m-%d-%y %H:%M:%S',)")
+            #print(int(utc.timestamp()) - 36000)
+            utcint = int(utc.timestamp()) - 36000
+            print (utcint)
+        except ValueError:
+            utc = datetime.strptime(date, "('%m/%d/%Y %H:%M:%S',)")
+            #print(int(utc.timestamp()) - 36000)
+            utcint = int(utc.timestamp()) - 36000
+            print (utcint)
+        StartListInt.append(utcint)
 
-for date in StartList:
-    try:
-        utc = datetime.strptime(date, '%m-%d-%y %H:%M:%S')
-        print(int(utc.timestamp()) - 36000)
-    except ValueError:
-        utc = datetime.strptime(date, '%m/%d/%Y %H:%M:%S')
-        print(int(utc.timestamp()) - 36000)
+    for date in EndList:
+        try:
+            utc = datetime.strptime(date, "('%m-%d-%y %H:%M:%S',)")
+            #print(int(utc.timestamp()) - 36000)
+            utcint = int(utc.timestamp()) - 36000
+            print (utcint)
+        except ValueError:
+            utc = datetime.strptime(date,"('%m/%d/%Y %H:%M:%S',)")
+            #print(int(utc.timestamp()) - 36000)
+            utcint = int(utc.timestamp()) - 36000
+            print (utcint)
+        EndListInt.append(utcint)
 
 
+
+"""(inputtime - starttime)/Unixdays"""
+def FindDayIntervals():
+    maxTime = backend.findMaxTime(con)
+    minTime = backend.findMinTime(con)
+    UnixDayValue=86400
+    indexes=int((maxTime-minTime)/UnixDayValue)
+    TimeIndexes=[]
+    for i in range(indexes+1):
+        x=minTime+i*UnixDayValue
+        TimeIndexes.append(x)
+    TimeIndexes.append(maxTime)
+    print(TimeIndexes)
+
+unix_Converter()
+print(StartListInt)
+print(EndListInt)
+FindDayIntervals()
--- backend.py
+++ backend.py
@@ -1,14 +1,25 @@
-from UnixConverter import unix_Converter
-unix_Converter()
+#from UnixConverter import unix_Converter
+#unix_Converter()
 import MySQLdb
+#import sqlite3
 
-con = MySQLdb.connect(host="pf.parsl.dev", user="hacc", passwd="hacc2019")
+con = MySQLdb.connect(db="hacc",host="pf.parsl.dev", user="hacc", passwd="hacc2019")
+#con = sqlite3.connect('shc.db')
 cursorObj = con.cursor()
-def makeTimeStamps():
-    max_time = cursorObj.execute("SELECT MAX([End Time]) FROM raw")
-    min_time =  cursorObj.execute("SELECT MIN([Start Time]) FROM raw")
-   # for i in max_time
 
+def findMaxTime(con):
+    con.query("SELECT MIN([Start_Time]) FROM raw")
+    maxTimeResult = con.store_result()
+    maxTime = maxTimeResult.fetch_row(maxrows=0)
+    return(maxTime)
 
-    print(min_time)
-    print(max_time)
+def findMinTime(con):
+    con.query("SELECT MAX([End_Time]) FROM raw")
+    minTimeResult = con.store_result()
+    minTime = minTimeResult.fetch_row(maxrows=0)
+    return(minTime)
+
+    '''max_time = cursorObj.execute("SELECT MAX([End_Time]) FROM raw")
+    min_time =  cursorObj.execute("SELECT MIN([Start_Time]) FROM raw")'''
+    print(minTime)
+    print(maxTime)
