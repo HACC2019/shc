c5bf5445a93825de14e2ea476fba4d985214e8d6
student <ibanez11173@gmail.com>
2019-11-06 14:05:56 -1000

    11/6

--- backend.py
+++ backend.py
@@ -149,3 +149,65 @@ def averageDataActually(column):
         totalAmount+=i
         totalValues+=1
     print(totalAmount/totalValues)
+
+
+#Gather by Time and Average by Column functions
+global starttime
+global endtime
+global rowDataList
+#given start + end time, find values between it, find avg
+#select avg kwh from raw where timestamp is less than or greater than __
+
+def gatherRows(starttime, endtime, con):
+    con.query(("SELECT * FROM raw WHERE Start_Time >= '{}' AND End_Time <= '{}'".format(starttime, endtime)))
+    rowData = con.store_result()
+    rowDataResults = rowData.fetch_row(maxrows=0)
+    rowDataList = []
+    for i in rowDataResults:
+        rowDataList.append(i)
+    print(rowDataList)
+    return rowDataList
+
+
+
+def averageData(column):
+    rowDataList = gatherRows(starttime, endtime, con)
+    columndata = []
+    numba=0
+    for i in rowDataList:
+        columndata.append(rowDataList[numba][column])
+        numba+=1
+    print(columndata)
+    return columndata
+
+def averageDataActually(column):
+    listOfValues=averageData(column)
+    totalAmount=0
+    totalValues=0
+    for i in listOfValues:
+        totalAmount+=i
+        totalValues+=1
+    print(totalAmount/totalValues)
+
+#Find TimeIntervals and add Timeintervals to proc table
+
+#Find Intervals
+def FindDayIntervals():
+    maxTime = findMaxTime(con)
+    minTime = findMinTime(con)
+    maxTime=int(maxTime[0][0])
+    minTime=int(minTime[0][0])
+    UnixDayValue=86400
+    indexes=int((maxTime-minTime)/UnixDayValue)+1
+    TimeIndexes=[]
+    for i in range(indexes):
+        x=minTime+i*UnixDayValue
+        TimeIndexes.append(x)
+    TimeIndexes.append(maxTime)
+    print(maxTime)
+    print(minTime)
+    print(TimeIndexes)
+    return(TimeIndexes)
+
+#Add TimeIntervals to proc table
+add_column(db=con,table='proc',column='TimeInterval',data=FindDayIntervals())
