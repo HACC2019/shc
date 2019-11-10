416eda5105494ed6ddf8e54bc2b430ee7c2fe628
Shane Parslow <shaneparslow808@gmail.com>
2019-11-09 15:34:44 -1000

    i was high when i added this inheritance

--- structures.py
+++ structures.py
@@ -9,17 +9,5 @@ class Problem:
     def __init__(self, starttime, endtime):
         self.problemName = None
         self.color = None
-
-
-class ProblemUsageDrop(Problem):
-    def __init__(self, starttime, endtime):
-        super().__init__(starttime, endtime)
-        self.problemName = "Usage Drop"
-        self.color = 0xFF0000
-
-
-class ProblemCongestion(Problem):
-    def __init__(self, starttime, endtime):
-        super().__init__(starttime, endtime)
-        self.problemName = "Congestion"
-        self.color = 0xFFFF00
+        self.problemStart = starttime
+        self.problemEnd = endtime
