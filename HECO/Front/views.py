e0d494739faf058ce6d7c1a83f4e2223ed41b520
kobeuyeda <kobeuyeda@outlook.com>
2019-11-11 10:34:06 -1000

    working on 7days graph

--- HECO/Front/views.py
+++ HECO/Front/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Raw_Data
+
+
 # Create your views here.
 def Home(request):
     name = ['i']
+    datas = []
+
+
+
+
     charge_station = Raw_Data.objects.all()
     ticker = 0
     for n in charge_station:
         for y in name:
             if (n.Charge_Station_Name == y):
                 ticker = ticker + 1
-                print(ticker, n.Charge_Station_Name)
-
         if ticker == 0:
-            print(name)
             name.append(n.Charge_Station_Name)
-
             if name[0] == 'i':
                 del name[0]
         ticker = 0
 
+    for r in name:
+        datas.append(Raw_Data.objects.filter(Charge_Station_Name=r))
+
+    timer = Raw_Data.objects.filter(Charge_Station_Name='A')
+
+    counter = 0
+
+    for f in timer:
+        if int(f.Start_Time) > int(counter):
+            counter = f.Start_Time
+
+    day = 86400
+    mintime = int(counter) - (day * 7)
 
     print(name)
 
-    return render(request, 'Homepage.html', {'station': name})
+    return render(request, 'Homepage.html', {'station': name, 'data': datas, 'time': timer, 'mintime': int(mintime)})
--- finding info.py
+++ finding info.py
@@ -1,18 +1,7 @@
 #from UnixConverter import unix_Converter
 from datetime import datetime
-#unix_Converter()
-import MySQLdb
 from statistics import mean
 import structures
-
-
-global meters  # List of structures.Meter objects
-meters = []
-global meterdict  # Dictionary of charger name vs 'meters' index
-meterdict = {}
-
-con = MySQLdb.connect(db="hacc",host="pf.parsl.dev", user="hacc", passwd="hacc2019")
-cursorObj = con.cursor()
 
 
 def findMinTime(con):
@@ -27,43 +16,6 @@ def findMaxTime(con):
     maxTimeResult = result
     maxTime = maxTimeResult.fetch_row(maxrows=0)
     return(maxTime)
-
-
-def add_column(db, table, column, data, dtype="TEXT"):
-    """Set a column equal to a list, creating the column if it doesn't exist"""
-
-    # Sanity checks #
-    if type(db) is not MySQLdb.connections.Connection:
-        raise TypeError("Expected MySQLdb.connections.connection, got {}".format(type(db)))
-    if type(table) is not str:
-        raise TypeError("Expected string, got {}".format(type(table)))
-    if type(column) is not str:
-        raise TypeError("Expected string, got {}".format(type(column)))
-    if type(data) is not list:
-        raise TypeError("Expected list, got {}".format(type(data)))
-
-    db.autocommit(on=True)  # Make sure to actually save changes
-
-    # Try to drop column if it exists #
-    try:
-        db.query("ALTER TABLE {} DROP COLUMN {}".format(table, column))
-    except MySQLdb._exceptions.OperationalError:
-        pass
-
-    # Find max row num to know when to stop updating and start inserting #
-    cur = db.cursor()
-    cur.execute("SELECT MAX(ID) FROM {}".format(table))
-    maxindex = cur.fetchall()
-
-    # Create column and add in data #
-    db.query("ALTER TABLE {} ADD {} {}".format(table, column, dtype))
-    i = 1
-    for value in data:  # Replace every row with data starting at ID 1
-        if i <= maxindex[0][0]:  # Update if row exists
-            db.query("UPDATE {} SET {}='{}' WHERE ID={}".format(table, column, value, i))
-        if i > maxindex[0][0]:  # Insert if row doesn't exist
-            db.query("INSERT INTO {} ({}) VALUES ('{}')".format(table, column, value))
-        i += 1
 
 #Unix Converter and stuff
 def sql_fetch_StartTimes(con):
